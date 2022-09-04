bb8c475c21a3072b7e272ee5ddefc6f7c8a149e9
Wonhee Jang <Qtsky89@gmail.com>
2022-09-05 00:20:36 +0900

    Done 97 page

--- chapter2/test.py
+++ chapter2/test.py
@@ -3,25 +3,28 @@ import sys
 from abc import ABC, abstractmethod
 from typing import List
 
+
 # interface
 class Observer(ABC):
     @abstractmethod
     def update(self, temperature: float, humidity: float, pressure: float):
         raise NotImplementedError
 
+
 # interface
 class Subject(ABC):
     @abstractmethod
-    def register_observer(self, o :Observer):
+    def register_observer(self, o: Observer):
         raise NotImplementedError
-    
+
     @abstractmethod
     def remove_observer(self, o: Observer):
         raise NotImplementedError
-    
+
     @abstractmethod
     def notify_observers(self):
         raise NotImplementedError
+
 
 # interface
 class DisplayElement(ABC):
@@ -37,24 +40,24 @@ class WeatherData(Subject):
         self.__temperature: float = 0
         self.__humidity: float = 0
         self.__pressure: float = 0
-        
-    
+
     def register_observer(self, o: Observer):
         self.__observers.append(o)
-    
+
     def remove_observer(self, o: Observer):
         self.__observers.remove(o)
-    
-    def notify_observer(self):
+
+    def notify_observers(self):
         for o in self.__observers:
             o.update(self.__temperature, self.__humidity, self.__pressure)
-    
+
     def set_mesasurements(self, temperature: float, humidity: float, pressure: float):
         self.__temperature = temperature
         self.__humidity = humidity
         self.__pressure = pressure
 
         self.notify_observers()
+
 
 class CurrentConditionDisplay(Observer, DisplayElement):
     def __init__(self, weather_data: WeatherData):
@@ -66,19 +69,25 @@ class CurrentConditionDisplay(Observer, DisplayElement):
 
         self.__weather_data = weather_data
         self.__weather_data.register_observer(self)
-    
+
     def update(self, temperature: float, humidity: float, pressure: float):
         self.__temperature = temperature
         self.__humidity = humidity
         self.display()
-    
+
     def display(self):
         print(f'temperature: {self.__temperature}, humidity: {self.__humidity}')
-    
- 
+
+
 def main(argv):
-    print('hello world')
-    
+    weather_data = WeatherData()
+
+    CurrentConditionDisplay(weather_data)
+
+    weather_data.set_mesasurements(80, 65, 30.4)
+    weather_data.set_mesasurements(82, 70, 29.2)
+    weather_data.set_mesasurements(78, 90, 29.2)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
