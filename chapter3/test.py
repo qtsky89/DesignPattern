1cd478ecb5d38f19a54957f8744fdf125f2443a1
Wonhee Jang <Qtsky89@gmail.com>
2022-09-06 01:07:42 +0900

    chatper3 done

--- chapter3/test.py
+++ chapter3/test.py
@@ -1,9 +1,68 @@
 #!/bin/env python
 import sys
+from abc import ABC, abstractmethod
+
+
+class Beverage(ABC):
+    def __init__(self):
+        self._description = 'no description'
+
+    def get_description(self) -> str:
+        return self._description
+
+    @abstractmethod
+    def cost(self) -> float:
+        raise NotImplementedError
+
+
+class Espresso(Beverage):
+    def __init__(self):
+        super().__init__()
+        self._description = 'espresso'
+
+    def cost(self) -> float:
+        return 1.99
+
+
+class HouseBlend(Beverage):
+    def __init__(self):
+        super().__init__()
+        self._description = 'houseblend'
+
+    def cost(self) -> float:
+        return 0.89
+
+
+class CondimentDecorator(Beverage):
+    # TODO: why I choose this architecture ??
+    def __init__(self):
+        self._beverage: Beverage = None
+
+    @abstractmethod
+    def get_description(self) -> str:
+        raise NotImplementedError
+
+
+class Mocha(CondimentDecorator):
+    def __init__(self, beverage: Beverage):
+        self._beverage = beverage
+
+    def get_description(self) -> str:
+        return f'{self._beverage.get_description()}, mocha'
+
+    def cost(self) -> float:
+        return self._beverage.cost() + 0.2
 
 
 def main(argv):
-    print('hello world')
+    b: Beverage = Espresso()
+
+    print(f'{b.get_description()}, ${b.cost()}')
+
+    b2: Beverage = HouseBlend()
+    b2 = Mocha(b2)
+
+    print(f'{b2.get_description()}, ${b2.cost()}')
 
 
 if __name__ == '__main__':
