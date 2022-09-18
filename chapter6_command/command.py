eb06a3c178eb449b72533f9ba2551c4bfa5416a3
Wonhee Jang <Qtsky89@gmail.com>
2022-09-18 16:52:29 +0900

    Added lightoff command

--- chapter6_command/command.py
+++ chapter6_command/command.py
@@ -15,3 +15,11 @@ class LightOnCommand (Command):
 
     def execute(self):
         self.light.on()
+
+
+class LightOffCommand (Command):
+    def __init__(self, light: Light):
+        self.light = light
+
+    def execute(self):
+        self.light.off()
--- chapter6_command/remote_control.py
+++ chapter6_command/remote_control.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 from command import Command
+from typing import List
 
 
 class SimpleRemoteControl:
@@ -11,3 +12,19 @@ class SimpleRemoteControl:
 
     def button_was_pressed(self):
         self.__slot.execute()
+
+
+class RemoteControl:
+    def __init__(self):
+        self.__on_commands: List[Command] = [None] * 7  # type: ignore
+        self.__off_commands: List[Command] = [None] * 7  # type: ignore
+
+    def set_command(self, slot: int, on_command: Command, off_command: Command):
+        self.__on_commands[slot] = on_command
+        self.__off_commands[slot] = off_command
+
+    def on_command_pushed(self, slot: int):
+        self.__on_commands[slot].execute()
+
+    def off_command_pushed(self, slot: int):
+        self.__off_commands[slot].execute()
