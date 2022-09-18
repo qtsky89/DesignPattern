fc00823cc20d7848f8d30a632c6b02a334dcb57e
Wonhee Jang <Qtsky89@gmail.com>
2022-09-18 19:07:38 +0900

    Added simple duck, turkey pattern

--- chapter7_adapter_facade/test.py
+++ chapter7_adapter_facade/test.py
@@ -1,9 +1,60 @@
 #!/bin/env python
 import sys
+from abc import ABCMeta, abstractmethod
+
+
+class Duck(metaclass=ABCMeta):
+    @abstractmethod
+    def quack(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def fly(self):
+        raise NotImplementedError
+
+
+class MallardDuck(Duck):
+    def quack(self):
+        print('quack quack')
+
+    def fly(self):
+        print('fly fly')
+
+
+class Turkey(metaclass=ABCMeta):
+    @abstractmethod
+    def gobble(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def fly(self):
+        raise NotImplementedError
+
+
+class WildTurkey(Turkey):
+    def gobble(self):
+        print('gobble gobble')
+
+    def fly(self):
+        print('fly')
+
+
+class TurkeyAdapter(Duck):
+    def __init__(self, turkey: Turkey) -> None:
+        self.__turkey = turkey
+
+    def quack(self):
+        self.__turkey.gobble()
+
+    def fly(self):
+        for _ in range(5):
+            self.__turkey.fly()
 
 
 def main(argv):
-    print('hello world')
+    t: Duck = TurkeyAdapter(WildTurkey())
+    t.quack()
+    t.fly()
 
 
 if __name__ == '__main__':
