37be8fd54362a187e70f302e138493b1c10e9b78
Wonhee Jang <Qtsky89@gmail.com>
2022-09-18 21:35:38 +0900

    Added iterator pattern

--- chapter9_iterator_composite/test.py
+++ chapter9_iterator_composite/test.py
@@ -1,5 +1,39 @@
 #!/bin/env python
+from abc import ABCMeta, abstractmethod
+from typing import List
 import sys
+
+
+class Iterator(metaclass=ABCMeta):
+    @abstractmethod
+    def has_next(self) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def next(self):
+        raise NotImplementedError
+
+
+class Menu(metaclass=ABCMeta):
+    @abstractmethod
+    def create_iterator() -> Iterator:
+        raise NotImplementedError
+
+
+class DinerMenuIterator(Iterator):
+    def __init__(self, menu: List[str]) -> None:
+        self.__menu = menu
+        self.__index = 0
+
+    def has_next(self) -> bool:
+        if self.__index + 1 <= len(self.__menu) - 1:
+            return True
+
+        return False
+
+    def next(self):
+        self.__index += 1
+        return self.__menu[self.__index]
 
 
 def main(argv):
