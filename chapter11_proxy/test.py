712b168507787eb9c2c07247b89eafe3d615e0d6
Wonhee Jang <Qtsky89@gmail.com>
2022-10-16 14:22:43 +0900

    Added test code in proxy

--- chapter11_proxy/test.py
+++ chapter11_proxy/test.py
@@ -2,8 +2,33 @@
 import sys
 
 
+class GumballMachine:
+    def __init__(self, location: str, count: int):
+        self.__location = location
+        self.__count = count
+
+    @property
+    def location(self) -> str:
+        return self.__location
+
+    @property
+    def count(self) -> int:
+        return self.__count
+
+
+class GumballMonitor:
+    def __init__(self, m: GumballMachine):
+        self.machine = m
+
+    def report(self):
+        print(f'location: {self.machine.location}')
+        print(f'count: {self.machine.count}')
+
+
 def main(argv):
-    print('hello world')
+    gm = GumballMachine('Seoul', 5)
+    monitor = GumballMonitor(gm)
+    monitor.report()
 
 
 if __name__ == '__main__':
